fb99fb93eb2dfa852dea41d060c24dfab2419179
Junwei Wang <i.junwei.wang@gmail.com>
2016-06-28 16:39:28 +0800

    Optimize the solution for problem 3 by limit the search space

--- 3/3.py
+++ 3/3.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from math import ceil, sqrt
 
 num = 600851475143
 max_prime = 1
+sqr = int(ceil(sqrt(num)))
 i = 3
-while i <= num:
+while i <= num and i < sqr:
     while num % i == 0:
         max_prime = i
         num /= i
     i += 2
+if num >= sqr:
+    max_prime = num
 print max_prime
