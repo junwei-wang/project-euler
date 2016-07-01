b76d52689d0427646854bb0ae4c3fe5de44eb53a
Junwei Wang <i.junwei.wang@gmail.com>
2016-07-01 17:22:56 +0800

    Solve 16th problem.

---
+++ 16/16.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from math import log
+
+x = 2
+y = 1000
+
+# get sum of exp of 2 = y
+t = y
+exp = 0
+l = list()
+while y > 0:
+    if y & 1:
+        l.append(exp)
+    exp += 1
+    y >>= 1
+
+# get mult
+mult = x         # x ^ 1 = x ^ (2^0)
+cur_exp = 0
+res = 1
+for exp in l:
+    while cur_exp < exp:
+        mult **= 2
+        cur_exp += 1
+    res *= mult
+
+print reduce(lambda x, y: int(x) + int(y), str(res))
