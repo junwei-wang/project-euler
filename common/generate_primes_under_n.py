e8163b8cd41194eb39df4f5ca097241c6b860ed0
Junwei Wang <i.junwei.wang@gmail.com>
2016-07-03 13:38:57 +0800

    Add common way to generate prime under a number

---
+++ common/generate_primes_under_n.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from math import sqrt, ceil
+
+def get_primes_under_n(num):
+    length = int(ceil(num/2.0))
+    sevie = [True] * length # 1, 3, 5, 7, ..., n-1
+    sevie[0] = False
+
+    # a number has at most one factor than its squre
+    sqr = int(ceil(sqrt(num)))
+    sqr_idx = (sqr+1)/2
+    for i in range(1, sqr_idx):
+        if sevie[i]:
+            start = 3*i + 1
+            step = 2*i + 1
+            for j in range(start, length, step):
+                sevie[j] = False
+
+    primes = [2*k+1 for k, v in enumerate(sevie) if v]
+    primes = [2] + primes
+
+    return primes
