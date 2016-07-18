a501effeb45f510d9720e9f507af213b30d3b9e6
Junwei Wang <i.junwei.wang@gmail.com>
2016-07-18 22:49:41 +0800

    Solve p069

---
+++ 69/69.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""
+In short, n/phi(n) = (p1*...*pn)/((p1-1)*...*(pn-1))
+"""
+
+from math import sqrt, ceil
+
+def is_prime(num, prime_list):
+    sqr = ceil(sqrt(num))
+    for p in prime_list:
+        if p > sqr:
+            return True
+        if num % p == 0:
+            return False
+    return True
+
+def generate_first_n_prime(n):
+    primes = [2, 3, 5, 7, 11, 13, 17, 19]
+    i = 18
+    while len(primes) < n:
+        i += 6
+        if is_prime(i-1, primes):
+            primes.append(i-1)
+        if is_prime(i+1, primes):
+            primes.append(i+1)
+    return primes
+
+primes = generate_first_n_prime(1000)
+res = 1
+limit = 1000000
+for p in primes:
+    res *= p
+    if res >= limit:
+        print res/p
+        break
